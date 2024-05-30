7d105c232f5874d7120fc64c68a15e0bc26cc12e
Aytac Mehdizadə <aytacmehdizade1121@gmail.com>
2024-05-30 15:26:04 +0400

    home-page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,10 @@ import photo2 from "../../assets/images/calendar-1-11.svg";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import ApexChart from '../../components/Chart';
 import CircleChart from '../../components/CircleChart';
-
+import { PiTelevisionSimpleLight } from "react-icons/pi";
 
 import "./home.css"
+import { Link } from "react-router-dom";
 
 const Home = () => {
 
@@ -48,7 +49,7 @@ const Home = () => {
                 <ApexChart />
                 <CircleChart />
             </div>
-            <div className="">
+            <section className="home-tasks-sec">
                 <div className="home-employee-task">
                     <div>
                         <p>İşçilərin performansı</p>
@@ -122,10 +123,142 @@ const Home = () => {
                         </ul>
                     </div>
                 </div>
-                <div>
-                    
+                <div className="home-tasks">
+                    <div>
+                        <p>Tapşırıqlar</p>
+                        <Link>Hamısına bax</Link>
+                    </div>
+                    <ul>
+                        <li>
+                            Ad
+                        </li>
+                        <li>
+                            Saat
+                        </li>
+                        <li>
+                            Növ
+                        </li>
+                        <li>
+                            Adres
+                        </li>
+                        <li>
+                            Nömrə
+                        </li>
+                        <li>
+                            Status
+                        </li>
+                    </ul>
+                    <div>
+                        <ul>
+                            <li>
+                                Nicat Hasanov
+                            </li>
+                            <li>
+                                16:00 - 18:00
+                            </li>
+                            <li>
+                                <PiTelevisionSimpleLight />
+
+                            </li>
+                            <li>
+                                Yasamal, Mirzə Ş..
+                            </li>
+                            <li>
+                                (051) 555 5555
+                            </li>
+                            <li>
+                                Qəbul edilib
+                            </li>
+
+                        </ul>
+                        <ul>
+                            <li>
+                                Nicat Hasanov
+                            </li>
+                            <li>
+                                16:00 - 18:00
+                            </li>
+                            <li>
+                                <PiTelevisionSimpleLight />
+
+                            </li>
+                            <li>
+                                Yasamal, Mirzə Ş..
+                            </li>
+                            <li>
+                                (051) 555 5555
+                            </li>
+                            <li>
+                                Qəbul edilib
+                            </li>
+
+                        </ul><ul>
+                            <li>
+                                Nicat Hasanov
+                            </li>
+                            <li>
+                                16:00 - 18:00
+                            </li>
+                            <li>
+                                <PiTelevisionSimpleLight />
+
+                            </li>
+                            <li>
+                                Yasamal, Mirzə Ş..
+                            </li>
+                            <li>
+                                (051) 555 5555
+                            </li>
+                            <li>
+                                Qəbul edilib
+                            </li>
+
+                        </ul><ul>
+                            <li>
+                                Nicat Hasanov
+                            </li>
+                            <li>
+                                16:00 - 18:00
+                            </li>
+                            <li>
+                                <PiTelevisionSimpleLight />
+
+                            </li>
+                            <li>
+                                Yasamal, Mirzə Ş..
+                            </li>
+                            <li>
+                                (051) 555 5555
+                            </li>
+                            <li>
+                                Qəbul edilib
+                            </li>
+
+                        </ul><ul>
+                            <li>
+                                Nicat Hasanov
+                            </li>
+                            <li>
+                                16:00 - 18:00
+                            </li>
+                            <li>
+                                <PiTelevisionSimpleLight />
+
+                            </li>
+                            <li>
+                                Yasamal, Mirzə Ş..
+                            </li>
+                            <li>
+                                (051) 555 5555
+                            </li>
+                            <li>
+                                Qəbul edilib
+                            </li>
+
+                        </ul>
+                    </div>
                 </div>
-            </div>
+            </section>
         </div>
     );
 };
