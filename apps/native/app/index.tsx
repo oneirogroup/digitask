a0ecdde1d8e5118ed6d5cbac0b34fc4411da0324
MDReal32 <veysaliyev00@gmail.com>
2024-09-09 21:32:09 +0400

    test(button): component testing
    Test button component

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,15 +1,21 @@
 import { Text, View } from "react-native";
 
+import { Button } from "@oneiro/ui-kit/native";
+
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
-        alignItems: "center",
+        alignItems: "center"
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+
+      <Button onPress={() => console.log("Pressed")}>
+        <Text>I'm button</Text>
+      </Button>
     </View>
   );
 }
