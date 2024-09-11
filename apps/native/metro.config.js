7a472997fdc1ce523dd8074d90f8c3a99812ba65
MDReal32 <veysaliyev00@gmail.com>
2024-09-11 09:43:34 +0400

    chore(tailwind): configuration file

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -12,4 +12,4 @@ config.watchFolders = [workspaceRoot];
 config.resolver.nodeModulesPaths = [resolve(projectRoot, "node_modules"), resolve(workspaceRoot, "node_modules")];
 config.resolver.disableHierarchicalLookup = true;
 
-module.exports = withNativeWind(config, { input: "./app/main.css", configPath: "tailwind.config.ts" });
+module.exports = withNativeWind(config, { input: "./app/main.css" });
