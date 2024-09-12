3518fa19002291025ff6fedb5daae1c8dd894a0d
MDReal32 <veysaliyev00@gmail.com>
2024-09-12 10:36:11 +0400

    chore(digitask): native app
    Update package name and test props

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,13 +1,13 @@
 import { Text } from "react-native";
 
-import { Button, View } from "@oneiro/ui-kit/native";
+import { Button, View } from "@oneiro/ui-kit";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Edit app/index.tsx to edit this screen.</Text>
 
-      <Button onPress={() => console.log("Pressed")}>
+      <Button onClick={() => console.log("Clicked")} className="bg-red-500">
         <Text>I'm button</Text>
       </Button>
     </View>
