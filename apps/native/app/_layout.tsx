b7441a963a29271bc63ae898f20bc4d1d7ee4e02
MDReal32 <veysaliyev00@gmail.com>
2024-09-21 14:54:43 +0400

    feat(native): pages

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -5,6 +5,9 @@ import "./main.css";
 export default function RootLayout() {
   return (
     <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
