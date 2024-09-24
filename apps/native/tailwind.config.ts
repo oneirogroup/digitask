afe120c95a25da7dce0a3a677100d642bd7dce0f
MDReal32 <veysaliyev00@gmail.com>
2024-09-24 10:15:08 +0400

    feat(digitask): tailwindcss configuration
    Add components to auto class generation

--- apps/native/tailwind.config.ts
+++ apps/native/tailwind.config.ts
@@ -1,6 +1,6 @@
 import { baseConfig } from "../../tailwind.config";
 
 export default baseConfig.extend({
-  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  content: ["./{app,components}/**/*.{js,jsx,ts,tsx}"],
   extra: { presets: [require("nativewind/preset")] }
 });
