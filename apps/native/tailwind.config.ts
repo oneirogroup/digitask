8a219b0d586279d835beedcdd1094d0d715da547
MDReal32 <veysaliyev00@gmail.com>
2024-10-02 18:02:40 +0400

    chore(tailwind): improve configuration

--- apps/native/tailwind.config.ts
+++ apps/native/tailwind.config.ts
@@ -1,6 +1,5 @@
 import { baseConfig } from "../../tailwind.config";
 
 export default baseConfig.extend({
-  content: ["./{app,components}/**/*.{js,jsx,ts,tsx}"],
-  extra: { presets: [require("nativewind/preset")] }
+  content: ["./{app,components}/**/*.{js,jsx,ts,tsx}"]
 });
