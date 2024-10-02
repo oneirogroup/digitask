424294dca7899cb0ec59430088bae4e38987daaf
MDReal32 <veysaliyev00@gmail.com>
2024-10-02 18:05:05 +0400

    feat(native): env var
    For ui-kit

--- apps/native/utils/env.ts
+++ apps/native/utils/env.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  EXPO_PUBLIC_API_URL: z.string().url()
+  EXPO_PUBLIC_API_URL: z.string().url(),
+  EXPO_PUBLIC_TAILWIND_CONFIG: z.string().optional()
 });
 
 export const env = envSchema.parse(process.env);
