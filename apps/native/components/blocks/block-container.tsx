b1d1982c1691fa632e6956c5eb7bf6df0373a344
MDReal32 <veysaliyev00@gmail.com>
2024-10-06 10:32:31 +0400

    feat(digitask): block-container
    Add ref to block container

--- apps/native/components/blocks/block-container.tsx
+++ apps/native/components/blocks/block-container.tsx
@@ -1,15 +1,22 @@
-import { FC, PropsWithChildren } from "react";
+import { ForwardRefRenderFunction, PropsWithChildren, forwardRef } from "react";
 
-import { Block, LinearGradient, cn } from "@oneiro/ui-kit";
+import { Block, BlockRef, LinearGradient, LinearGradientRef, cn } from "@oneiro/ui-kit";
 
 import { BlockContainerProps } from "./block-container.types";
 
-export const BlockContainer: FC<PropsWithChildren<BlockContainerProps>> = ({ className, gradient, children }) => {
+const BlockContainerBase: ForwardRefRenderFunction<
+  BlockRef | LinearGradientRef,
+  PropsWithChildren<BlockContainerProps>
+> = ({ className, gradient, children }, ref) => {
   return gradient ? (
-    <LinearGradient className={className} {...gradient}>
+    <LinearGradient ref={ref} className={className} {...gradient}>
       {children}
     </LinearGradient>
   ) : (
-    <Block className={cn("rounded-2xl bg-white p-4", className)}>{children}</Block>
+    <Block ref={ref} className={cn("rounded-2xl bg-white p-4", className)}>
+      {children}
+    </Block>
   );
 };
+
+export const BlockContainer = forwardRef(BlockContainerBase);
