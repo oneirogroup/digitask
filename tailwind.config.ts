72316bc069590e6efbeef44ab810f080a01d32ab
MDReal32 <veysaliyev00@gmail.com>
2024-10-06 10:17:53 +0400

    feat(digitask): tailwind config
    Add spin animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,12 @@ export const baseConfig = defineConfig({
       extend: {
         fontSize: {
           "1.5xl": "1.375rem"
+        },
+        animation: {
+          spin: "spin 1s linear infinite"
+        },
+        keyframes: {
+          spin: { "0%": { transform: "rotate(0deg)" }, "100%": { transform: "rotate(360deg)" } }
         }
       }
     }
