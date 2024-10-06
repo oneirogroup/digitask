9d7dcd24a3f2e8b83a0fc94970cf8bf9ce02660e
MDReal32 <veysaliyev00@gmail.com>
2024-10-06 10:24:32 +0400

    fix(digitask): auth/_layout
    set false to freezeOnBlur cause error

--- apps/native/app/(auth)/_layout.tsx
+++ apps/native/app/(auth)/_layout.tsx
@@ -9,9 +9,9 @@ AuthHttp.setBaseUrl(env.EXPO_PUBLIC_API_URL);
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-      <Stack.Screen name="forgot-password" options={{ headerShown: false }} />
-      <Stack.Screen name="code" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false, freezeOnBlur: false }} />
+      <Stack.Screen name="forgot-password" options={{ headerShown: false, freezeOnBlur: false }} />
+      <Stack.Screen name="code" options={{ headerShown: false, freezeOnBlur: false }} />
     </Stack>
   );
 }
