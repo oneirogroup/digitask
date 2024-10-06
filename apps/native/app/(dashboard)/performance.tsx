e7e930843927aeaff613c561776ce0805954c76d
MDReal32 <veysaliyev00@gmail.com>
2024-10-06 10:24:09 +0400

    fix(digitask): dashboard/performance
    Performance tab is ready/50%

--- apps/native/app/(dashboard)/performance.tsx
+++ apps/native/app/(dashboard)/performance.tsx
@@ -1,11 +1,24 @@
-import { Text } from "react-native";
+import { Block, Table, TableRef, useTableControls } from "@oneiro/ui-kit";
+import { useMutation } from "@tanstack/react-query";
 
-import { View } from "@oneiro/ui-kit";
+import { DateRange, RangePicker } from "../../components/date-time-picker";
 
 export default function Performance() {
+  const tableControls = useTableControls<TableRef>();
+
+  const rangeTableMutation = useMutation({
+    mutationKey: ["performance/range-table"],
+    mutationFn: (range: DateRange) => new Promise<never[]>(resolve => setTimeout(resolve, 1500, [])),
+    onSuccess(data) {
+      console.log(data);
+    }
+  });
+
   return (
-    <View>
-      <Text>Hello, Performance!</Text>
-    </View>
+    <Block className="relative p-4">
+      <RangePicker onChange={range => rangeTableMutation.mutate(range)} />
+
+      <Table ref={tableControls.ref} />
+    </Block>
   );
 }
