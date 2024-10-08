c87a47645a75c7d59530f9acb111898dc466d545
MDReal32 <veysaliyev00@gmail.com>
2024-10-08 22:35:01 +0400

    feat(digitask): performance page
    Add example view of page

--- apps/native/app/(dashboard)/performance.tsx
+++ apps/native/app/(dashboard)/performance.tsx
@@ -1,24 +1,98 @@
-import { Block, Table, TableRef, useTableControls } from "@oneiro/ui-kit";
+import { FC, useEffect } from "react";
+
+import { Block, If, Table, Text } from "@oneiro/ui-kit";
 import { useMutation } from "@tanstack/react-query";
 
 import { DateRange, RangePicker } from "../../components/date-time-picker";
+import { DateService } from "../../services/date-service";
+
+const Loading: FC = () => <Text className="text-center">Loading...</Text>;
 
 export default function Performance() {
-  const tableControls = useTableControls<TableRef>();
-
-  const rangeTableMutation = useMutation({
+  const {
+    data: rangeTableData = [],
+    isPending: isRangeTablePending,
+    mutate: rangeTableMutate,
+    isSuccess: isRangeTableSuccess,
+    isError: isRangeTableError
+  } = useMutation({
     mutationKey: ["performance/range-table"],
-    mutationFn: (range: DateRange) => new Promise<never[]>(resolve => setTimeout(resolve, 1500, [])),
-    onSuccess(data) {
-      console.log(data);
-    }
+    mutationFn: (range: DateRange) =>
+      new Promise<{ id: number; name: string; group: string; tasks: number }[]>(resolve =>
+        setTimeout(
+          resolve,
+          1500,
+          Array.from({ length: 100 }, (_, idx) => ({ id: idx + 1, name: "John Doe", tasks: (idx + 1) * 10 }))
+        )
+      ),
+    onSuccess(data) {}
   });
 
-  return (
-    <Block className="relative p-4">
-      <RangePicker onChange={range => rangeTableMutation.mutate(range)} />
+  useEffect(() => {
+    rangeTableMutate({ start: DateService.from(new Date()), end: DateService.from(new Date()) });
+  }, []);
 
-      <Table ref={tableControls.ref} />
-    </Block>
+  return (
+    <Block.Scroll className="relative flex max-h-full gap-4 p-4">
+      <RangePicker onChange={range => rangeTableMutate(range)} />
+
+      <Table>
+        <Table.Header>
+          <Table.Header.Cell name="name">
+            <Text className="text-center">Name</Text>
+          </Table.Header.Cell>
+          <Table.Header.Cell name="group">
+            <Text className="text-center">Group</Text>
+          </Table.Header.Cell>
+          <Table.Header.Cell name="tasks">
+            <Text className="text-center">Tasks</Text>
+          </Table.Header.Cell>
+        </Table.Header>
+
+        <Table.Body>
+          <If condition={isRangeTablePending}>
+            <If.Then>
+              <Table.Row>
+                <Table.Cell fullCellSpan>
+                  <Loading />
+                </Table.Cell>
+              </Table.Row>
+            </If.Then>
+
+            <If.ElseIf condition={isRangeTableSuccess}>
+              {rangeTableData.map(({ id, name, group, tasks }) => (
+                <Table.Row key={id}>
+                  <Table.Cell>
+                    <Text className="text-center">{name}</Text>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Text className="text-center">{group}</Text>
+                  </Table.Cell>
+                  <Table.Cell>
+                    <Text className="text-center">{tasks}</Text>
+                  </Table.Cell>
+                </Table.Row>
+              ))}
+            </If.ElseIf>
+
+            <If.ElseIf condition={isRangeTableError}>
+              <Table.Row fullRowSpan>
+                <Table.Cell>
+                  <Text>Error</Text>
+                </Table.Cell>
+              </Table.Row>
+            </If.ElseIf>
+
+            <If.Else>
+              <Table.Row fullRowSpan>
+                <Table.Cell fullCellSpan>
+                  <Text>No data</Text>
+                </Table.Cell>
+              </Table.Row>
+            </If.Else>
+          </If>
+        </Table.Body>
+      </Table>
+    </Block.Scroll>
   );
 }
--- apps/native/components/date-time-picker/range-picker.tsx
+++ apps/native/components/date-time-picker/range-picker.tsx
@@ -1,21 +1,24 @@
 import { FC, useRef, useState } from "react";
-import { useClickOutside } from "react-native-click-outside";
+import { Dimensions } from "react-native";
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import DateTimePicker from "react-native-ui-datepicker";
 import { RangeChange } from "react-native-ui-datepicker/src/types";
 import { useBoolean } from "react-use";
 
-import { Button, Icon, Text, View, ViewRef, When } from "@oneiro/ui-kit";
+import { Button, Icon, Text, View } from "@oneiro/ui-kit";
 
 import { DateService } from "../../services/date-service";
 import { BlockContainer } from "../blocks";
 import { DateRange, RangePickerProps } from "./range-picker.types";
+
+const { height: screenHeight } = Dimensions.get("window");
 
 export const RangePicker: FC<RangePickerProps> = ({ onChange }) => {
   const [isDatePickerVisible, toggleDatePickerVisibility] = useBoolean(false);
   const [range, setRange] = useState<Partial<DateRange> | null>(null);
   const [debouncedRange, setDebouncedRange] = useState<Partial<DateRange> | null>(null);
   const tmpStartDate = useRef<DateService | null>(null);
-  const ref = useClickOutside<ViewRef>(toggleDatePickerVisibility);
+  const height = useSharedValue(-screenHeight * 0.4);
 
   const handleDateChange: RangeChange = ({ startDate, endDate }) => {
     if (startDate && tmpStartDate.current && !endDate) {
@@ -47,11 +50,16 @@ export const RangePicker: FC<RangePickerProps> = ({ onChange }) => {
     if (!isDatePickerVisible && range?.start && range?.end) {
       setRange(null);
     }
+    height.value = isDatePickerVisible ? 0 : -screenHeight * 0.4;
     toggleDatePickerVisibility();
   };
 
+  const animatedStyle = useAnimatedStyle(() => ({
+    bottom: withTiming(height.value, { duration: 300 })
+  }));
+
   return (
-    <BlockContainer>
+    <BlockContainer className="relative">
       <Button variant="none" onClick={toggleDatePicker} className="flex flex-row justify-between">
         <View className="flex flex-row gap-2">
           <View>
@@ -67,11 +75,73 @@ export const RangePicker: FC<RangePickerProps> = ({ onChange }) => {
         <Icon name="calendar" state="raw" variables={{ fill: "black" }} />
       </Button>
 
-      <When condition={isDatePickerVisible}>
-        <View ref={ref} className="absolute left-4 top-24 rounded-2xl bg-white">
-          <DateTimePicker mode="range" startDate={range?.start} endDate={range?.end} onChange={handleDateChange} />
-        </View>
-      </When>
+      <View className="absolute bottom-0 left-0 right-0">
+        <Animated.View style={animatedStyle} className="absolute left-0 right-0">
+          <View className="">
+            <DateTimePicker mode="range" startDate={range?.start} endDate={range?.end} onChange={handleDateChange} />
+          </View>
+        </Animated.View>
+      </View>
+
+      {/*<View className="flex-1 justify-end">*/}
+      {/*<AnimatedView></AnimatedView>*/}
+
+      {/*<When condition={isDatePickerVisible}>*/}
+      {/*  <Button className="absolute inset-0 bg-black/50" onPress={toggleDatePicker} />*/}
+      {/*</When>*/}
+      {/*<AnimatedView*/}
+      {/*  style={[{ height: screenHeight * 0.6 }, animatedStyle]}*/}
+      {/*  className="w-full rounded-t-3xl bg-white shadow-lg"*/}
+      {/*>*/}
+      {/*  <DateTimePicker mode="range" startDate={range?.start} endDate={range?.end} onChange={handleDateChange} />*/}
+      {/*</AnimatedView>*/}
+      {/*</View>*/}
+
+      {/*<Modal*/}
+      {/*  animationType="slide"*/}
+      {/*  transparent={true}*/}
+      {/*  visible={isDatePickerVisible}*/}
+      {/*  onRequestClose={toggleDatePickerVisibility}*/}
+      {/*>*/}
+      {/*<TouchableWithoutFeedback onPress={toggleDatePicker}>*/}
+      {/*  <View className="flex-1 items-center justify-end bg-black/50">*/}
+      {/*    <TouchableWithoutFeedback>*/}
+      {/*      <View className="h-max w-full rounded-t-2xl bg-white">*/}
+      {/*        <DateTimePicker*/}
+      {/*          mode="range"*/}
+      {/*          startDate={range?.start}*/}
+      {/*          endDate={range?.end}*/}
+      {/*          onChange={handleDateChange}*/}
+      {/*        />*/}
+      {/*      </View>*/}
+      {/*    </TouchableWithoutFeedback>*/}
+      {/*  </View>*/}
+      {/*</TouchableWithoutFeedback>*/}
+
+      {/*<Button*/}
+      {/*  variant="none"*/}
+      {/*  className="flex-1 items-center justify-end bg-black/50"*/}
+      {/*  onClick={toggleDatePickerVisibility}*/}
+      {/*>*/}
+      {/*  <Button variant="none" onClick={e => e.stopPropagation()} className="h-max w-full rounded-t-2xl bg-white">*/}
+      {/*  </Button>*/}
+      {/*</Button>*/}
+
+      {/*<TouchableWithoutFeedback onPress={toggleDatePickerVisibility}>*/}
+      {/*<View>*/}
+      {/*  <TouchableWithoutFeedback>*/}
+      {/*    <View >*/}
+
+      {/*    </View>*/}
+      {/*  </TouchableWithoutFeedback>*/}
+      {/*</View>*/}
+      {/*</TouchableWithoutFeedback>*/}
+      {/*</Modal>*/}
+      {/*<When condition={isDatePickerVisible}>*/}
+      {/*  <View ref={ref} className="absolute left-4 top-24 rounded-2xl bg-white">*/}
+
+      {/*  </View>*/}
+      {/*</When>*/}
     </BlockContainer>
   );
 };
