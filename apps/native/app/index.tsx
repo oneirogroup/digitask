7a631a0343ad9a2337212ee70feaf9a18340c633
MDReal32 <veysaliyev00@gmail.com>
2024-10-12 20:13:07 +0400

    feat(digitask): index
    Fetch profile. Check token then redirect to preferred page

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -2,27 +2,37 @@ import { Image } from "expo-image";
 import { router } from "expo-router";
 import { useEffect } from "react";
 
-import { Block, Text, View, cn } from "@oneiro/ui-kit";
+import { AuthHttp, Block, Text, View, cn } from "@oneiro/ui-kit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useMutation } from "@tanstack/react-query";
 
 import { Tokens } from "../types/tokens";
-import { sleep } from "../utils/sleep";
+import { cache } from "../utils/cache";
 
 import logo from "../assets/images/logo.png";
 
 export default function Index() {
+  const profileMutation = useMutation({
+    mutationKey: [cache.user.profile.$value],
+    mutationFn: () => AuthHttp.instance().get("/accounts/profile/")
+  });
+
   useEffect(() => {
-    sleep(1)
-      .then(() => AsyncStorage.getItem(Tokens.ACCESS_TOKEN))
-      .then(token => {
-        if (!token) return Promise.reject();
-        router.replace("/(dashboard)");
-      })
+    const authHttpSettings = AuthHttp.settings();
+
+    authHttpSettings
+      .setStorage(AsyncStorage)
+      .setStorageTokenKeys({ access: Tokens.ACCESS_TOKEN, refresh: Tokens.REFRESH_TOKEN })
+      .setRefreshUrl("/accounts/token/refresh/")
+      .waitForToken({ timeout: 1e5 })
+      .then(authHttpSettings.retrieveTokens())
+      .then(profileMutation.mutateAsync)
+      .then(() => router.replace("/(dashboard)"))
       .catch(() => router.replace("/welcome"));
   }, []);
 
   return (
-    <View className={cn("bg-primary h-full w-full", "flex items-center pt-60")}>
+    <View className={cn("flex items-center pt-60", "bg-primary h-full w-full")}>
       <Block className="flex items-center gap-8">
         <Image source={logo} style={{ width: 180, height: 169 }} />
         <Text className="text-4xl text-white">Digi Task</Text>
