3ca38e57cdcac1741115370a10c9d70606bedd9f
MDReal32 <veysaliyev00@gmail.com>
2024-10-13 09:19:01 +0400

    feat(digitask): remove client

--- apps/client/next.config.mjs
+++
@@ -1,6 +0,0 @@
-import { withExpo } from "@expo/next-adapter";
-
-export default withExpo({
-  transpilePackages: ["nativewind", "react-native-css-interop", "react-native-svg", "react-native-svg-web"],
-  experimental: { reactCompiler: true, typedRoutes: true }
-});
--- apps/client/tailwind.config.ts
+++
@@ -1,6 +0,0 @@
-import { baseConfig } from "../../tailwind.config";
-
-export default baseConfig.extend({
-  content: ["./{components,app}/**/*.{js,ts,jsx,tsx,mdx}"],
-  extra: { important: "html", presets: [require("nativewind/preset")] }
-});
