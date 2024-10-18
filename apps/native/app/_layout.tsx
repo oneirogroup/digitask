16782e1cdd2d85cd6edc8939ce6cfc146b4302d0
MDReal32 <veysaliyev00@gmail.com>
2024-10-18 14:38:24 +0400

    chore(digitask): chat
    Right header element for chat page

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,5 +1,6 @@
 import { Stack } from "expo-router";
 
+import { ChatHeaderRight } from "../components/header/chat/chat-header-right";
 import { Providers } from "../components/providers";
 
 import "./main.css";
@@ -10,7 +11,15 @@ export default function RootLayout() {
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false, freezeOnBlur: false }} />
         <Stack.Screen name="welcome" options={{ headerShown: false, freezeOnBlur: false }} />
-        <Stack.Screen name="chat" options={{ freezeOnBlur: false }} />
+        <Stack.Screen
+          name="chat"
+          options={{
+            title: "Chat",
+            headerBackTitleVisible: false,
+            freezeOnBlur: false,
+            headerRight: () => <ChatHeaderRight />
+          }}
+        />
         <Stack.Screen name="(auth)" options={{ headerShown: false, freezeOnBlur: false }} />
         <Stack.Screen name="(dashboard)" options={{ headerShown: false, freezeOnBlur: false }} />
       </Stack>
