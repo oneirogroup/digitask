0bff3b10807be74fb2d0bbb6017847c99db61758
MDReal32 <veysaliyev00@gmail.com>
2024-10-18 14:38:57 +0400

    feat(digitask): chat
    View is ready

--- apps/native/app/chat.tsx
+++ apps/native/app/chat.tsx
@@ -1,7 +1,6 @@
 import { Text, View } from "react-native";
 
-import { AuthHttp, Input, logger } from "@oneiro/ui-kit";
-import { useListen, useWebsocket } from "@oneiro/ws-client";
+import { Block, Input } from "@oneiro/ui-kit";
 import { useQuery } from "@tanstack/react-query";
 
 import { ProfileData } from "../types/backend/profile-data";
@@ -10,19 +9,42 @@ import { cache } from "../utils/cache";
 interface Message {}
 
 export default function Chat() {
-  const { data } = useQuery<ProfileData>({ queryKey: [cache.user.profile.$value], initialData: {} as any });
-  const tokens = AuthHttp.settings().getTokens();
-
-  logger.debug("digitask.native:chat:url", `ws://135.181.42.192/chat/?email=${data.email}&token=${tokens.access}`);
-  useWebsocket("chat", `ws://135.181.42.192/chat/?email=${data.email}&token=${tokens.access}`);
-  const messages = useListen<Message, true>("chat", true);
+  const { data } = useQuery<ProfileData>({ queryKey: [cache.user.profile.$value] });
+  if (!data) return null;
+  // const tokens = AuthHttp.settings().getTokens();
+  // logger.debug("digitask.native:chat:url", `ws://135.181.42.192/chat/?email=${data.email}&token=${tokens.access}`);
+  // useWebsocket("chat", `ws://135.181.42.192/chat/?email=${data.email}&token=${tokens.access}`);
+  // const messages = useListen<Message, true>("chat", true);
 
   return (
-    <View>
-      <Text>Chat</Text>
-
-      <Input />
-      <View></View>
-    </View>
+    <Block.Scroll>
+      <View className="px-6 py-3">
+        <Input
+          placeholder="Axtar"
+          variant="secondary"
+          className="placeholder:text-neutral"
+          icon={{ left: "search", right: "filter" }}
+        />
+      </View>
+      <Block className="px-4 py-3">
+        <View className="flex flex-row items-center gap-4">
+          <View className="bg-neutral-95 flex h-12 w-12 items-center justify-center rounded-full">
+            <Text className="text-primary">E</Text>
+          </View>
+          <View className="flex flex-1 gap-2">
+            <View className="flex flex-row justify-between">
+              <Text>Elmar Hasanov</Text>
+              <Text className="text-neutral-20">12m</Text>
+            </View>
+            <View className="flex flex-row justify-between">
+              <Text className="text-neutral">Sualim var</Text>
+              <View className="bg-success-60 flex h-5 w-5 items-center justify-center rounded-full text-white">
+                <Text className="text-xs text-white">2</Text>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Block>
+    </Block.Scroll>
   );
 }
