bae509ff9a8615865f223bdd621e36d4525d9df9
MDReal32 <veysaliyev00@gmail.com>
2024-10-18 14:38:05 +0400

    chore(digitask): dashboard layout header items

--- apps/native/components/header/dashboard-layout/header-right.tsx
+++ apps/native/components/header/dashboard-layout/header-right.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { FC } from "react";
 
 import { Block, Icon } from "@oneiro/ui-kit";
@@ -5,7 +6,9 @@ import { Block, Icon } from "@oneiro/ui-kit";
 export const HeaderRight: FC = () => {
   return (
     <Block className="flex flex-row items-center justify-end gap-4 pr-4">
-      <Icon name="chat" variables={{ stroke: "#005ABF" }} />
+      <Link href="/chat">
+        <Icon name="chat" variables={{ stroke: "#005ABF" }} />
+      </Link>
       <Icon name="notifications" variables={{ stroke: "#005ABF", checkmark: "none" }} />
     </Block>
   );
