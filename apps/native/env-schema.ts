0c348ec62f153e46025900ffeb69a9c48486b55d
MDReal32 <veysaliyev00@gmail.com>
2024-11-26 17:01:02 +0400

    fix(digitask): native
    Fix env-schema

--- apps/native/env-schema.ts
+++ apps/native/env-schema.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 
 const envSchema = z.object({
-  EXPO_PUBLIC_API_URL: z.string().url(),
-  EXPO_PUBLIC_TAILWIND_CONFIG: z.string().optional()
+  EXPO_PUBLIC_API_URL: z.string().url().default("http://135.181.42.192")
 });
 
 export const env = envSchema.parse(process.env);
