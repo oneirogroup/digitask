b998ea01017bef313aba3a038598835616cf3d23
MDReal32 <veysaliyev00@gmail.com>
2025-03-16 15:13:00 +0400

    feat: simplify date formatting logic in task-date component

--- apps/native/components/task/task/components/task-date.tsx
+++ apps/native/components/task/task/components/task-date.tsx
@@ -9,20 +9,10 @@ export const TaskDate: FC<TaskDateProps> = ({ start, end }) => {
   const startDate = DateService.from(start);
   const endDate = DateService.from(end);
   const dateParts: string[] = [];
-  const isDatesInSameDay = startDate.isSame(endDate, "day");
 
-  if (isDatesInSameDay) {
-    dateParts.push("Bu gün,");
-    dateParts.push(startDate.format("hh:mm"));
-    if (endDate) {
-      dateParts.push("-");
-      dateParts.push(endDate.format("hh:mm"));
-    }
-  } else {
-    dateParts.push(startDate.format("DD.MM.YYYY hh:mm"));
-    dateParts.push("-");
-    dateParts.push(endDate.format("DD.MM.YYYY hh:mm"));
-  }
+  dateParts.push(startDate.format("DD.MM.YYYY hh:mm"));
+  dateParts.push("-");
+  dateParts.push(endDate.format("DD.MM.YYYY hh:mm"));
 
   return <Text>{dateParts.join(" ")}</Text>;
 };
