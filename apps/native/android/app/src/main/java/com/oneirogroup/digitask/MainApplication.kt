6f2988bb6d34079f62c89133d73afd5c27e386b9
MDReal32 <veysaliyev00@gmail.com>
2025-04-06 13:14:09 +0400

    chore: cleanup

--- apps/native/android/app/src/main/java/com/oneirogroup/digitask/MainApplication.kt
+++ apps/native/android/app/src/main/java/com/oneirogroup/digitask/MainApplication.kt
@@ -22,10 +22,7 @@ class MainApplication : Application(), ReactApplication {
         this,
         object : DefaultReactNativeHost(this) {
           override fun getPackages(): List<ReactPackage> {
-            val packages = PackageList(this).packages
-            // Packages that cannot be autolinked yet can be added manually here, for example:
-            // packages.add(new MyReactNativePackage());
-            return packages
+            return PackageList(this).packages
           }
 
           override fun getJSMainModuleName(): String = ".expo/.virtual-metro-entry"
@@ -44,7 +41,6 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
     ApplicationLifecycleDispatcher.onApplicationCreate(this)
