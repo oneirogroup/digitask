b317b88de9e17f0c478d7969da01834fd8080d28
MDReal32 <veysaliyev00@gmail.com>
2025-05-05 08:22:15 +0400

    feat: initialize iOS project structure with Expo integration and essential configurations

--- apps/native/scripts/build-ios.sh
+++ apps/native/scripts/build-ios.sh
@@ -7,18 +7,40 @@ if [[ "$mode" != "debug" && "$mode" != "release" ]]; then
   echo "Invalid mode: $mode. Must be 'debug' or 'release'."
   exit 1
 fi
+ios_mode="$(tr '[:lower:]' '[:upper:]' <<< "${mode:0:1}")${mode:1}"
 
-if [ ! -d "android" ]; then
-  yarn expo prebuild -p ios || (yarn && yarn expo prebuild -p ios) || exit 1
+
+# check variables CODE_SIGN_IDENTITY and DEVELOPMENT_TEAM are set
+#if [[ -z "$CODE_SIGN_IDENTITY" || -z "$DEVELOPMENT_TEAM" ]]; then
+#  echo "Please set IOS_SIGN and IOS_TEAM_ID environment variables."
+#  exit 1
+#fi
+
+if [ ! -d "ios" ]; then
+  yarn expo prebuild -p ios --no-install || (yarn && yarn expo prebuild -p ios --no-install) || exit 1
 fi
 
 pushd ./ios || exit 1
-pod install || exit 1
-xcodebuild -workspace DigiTask.xcworkspace \
+if [ ! -d "Pods" ]; then
+  echo "📦 Pods didn't found. Installing..."
+  pod install || exit 1
+fi
+
+xcodebuild -downloadPlatform iOS
+  
+xcodebuild archive \
+  -workspace DigiTask.xcworkspace \
   -scheme DigiTask \
-  -configuration "$(tr '[:lower:]' '[:upper:]' <<< "${mode:0:1}")${mode:1}" \
   -sdk iphoneos \
-  -derivedDataPath build || exit 1
+  -configuration Release \
+  -archivePath build/DigiTask.xcarchive \
+  CODE_SIGN_STYLE=Automatic \
+  DEVELOPMENT_TEAM="$DEVELOPMENT_TEAM" || exit 1
+
+xcodebuild -exportArchive \
+  -archivePath build/DigiTask.xcarchive \
+  -exportOptionsPlist ../exportOptions.plist \
+  -exportPath build/ipa || exit 1
 
 popd || exit 1
 
@@ -31,7 +53,5 @@ if [[ -z "$APP_PATH" ]]; then
   echo "Failed to find .app file."
   exit 1
 fi
-
-xcrun -sdk iphoneos PackageApplication -v "$APP_PATH" -o "$(pwd)/build/ios.ipa" || exit 1
 
 echo "iOS build completed: build/ios.ipa"
