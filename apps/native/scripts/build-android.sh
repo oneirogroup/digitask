41a18e805129ee6b7843e3ac8e01db5f45c9f0f0
MDReal32 <veysaliyev00@gmail.com>
2025-05-26 20:09:15 +0400

    feat: initialize project structure with Expo and Fastlane configuration

--- apps/native/scripts/build-android.sh
+++ apps/native/scripts/build-android.sh
@@ -9,6 +9,10 @@ if [[ "$mode" != "debug" && "$mode" != "release" ]]; then
 fi
 gradle_mode="$(tr '[:lower:]' '[:upper:]' <<< "${mode:0:1}")${mode:1}"
 
+if [ ! -d "android" ]; then
+  yarn expo prebuild -p android --no-install || (yarn && yarn expo prebuild -p android --no-install) || exit 1
+fi
+
 pushd ./android || exit 1
 rm -rf app/build/generated/assets/createBundleReleaseJsAndAssets/index.android.bundle
 ./gradlew build "assemble${gradle_mode}" || exit 1
--- apps/native/scripts/build-ios.sh
+++ apps/native/scripts/build-ios.sh
@@ -7,31 +7,31 @@ if [[ "$mode" != "debug" && "$mode" != "release" ]]; then
   echo "Invalid mode: $mode. Must be 'debug' or 'release'."
   exit 1
 fi
+ios_mode="$(tr '[:lower:]' '[:upper:]' <<< "${mode:0:1}")${mode:1}"
 
-if [ ! -d "android" ]; then
-  yarn expo prebuild -p ios || (yarn && yarn expo prebuild -p ios) || exit 1
+
+if [ ! -d "ios" ]; then
+  yarn expo prebuild -p ios --no-install || (yarn && yarn expo prebuild -p ios --no-install) || exit 1
 fi
 
 pushd ./ios || exit 1
-pod install || exit 1
-xcodebuild -workspace DigiTask.xcworkspace \
-  -scheme DigiTask \
-  -configuration "$(tr '[:lower:]' '[:upper:]' <<< "${mode:0:1}")${mode:1}" \
-  -sdk iphoneos \
-  -derivedDataPath build || exit 1
+if [ ! -d "Pods" ]; then
+  echo "📦 Pods didn't found. Installing..."
+  pod install || exit 1
+fi
+
+fastlane build
 
 popd || exit 1
 
-mkdir -p build
-rm -rf build/ios.ipa
-
-APP_PATH=$(find ios/build/Build/Products -name '*.app' | head -n 1)
-
-if [[ -z "$APP_PATH" ]]; then
-  echo "Failed to find .app file."
-  exit 1
-fi
-
-xcrun -sdk iphoneos PackageApplication -v "$APP_PATH" -o "$(pwd)/build/ios.ipa" || exit 1
-
-echo "iOS build completed: build/ios.ipa"
+#mkdir -p build
+#rm -rf build/ios.ipa
+#
+#APP_PATH=$(find ios/build/Build/Products -name '*.app' | head -n 1)
+#
+#if [[ -z "$APP_PATH" ]]; then
+#  echo "Failed to find .app file."
+#  exit 1
+#fi
+#
+#echo "iOS build completed: build/ios.ipa"
